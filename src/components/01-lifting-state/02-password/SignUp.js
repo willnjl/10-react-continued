0433ecc841491fb22a1b46f93762d7c45b0ec1a0
Will <will_leighton@outlook.com>
2020-09-01 18:16:17 +0100

    password refactor

--- src/components/01-lifting-state/02-password/SignUp.js
+++ src/components/01-lifting-state/02-password/SignUp.js
@@ -14,6 +14,10 @@ export default class SignUp extends Component {
   render() {
     const { field01, field02 } = this.state;
     const { minimumLength } = this.props;
+    const valid =
+      field01 === field02 &&
+      field01.length + field02.length >= minimumLength * 2;
+
     return (
       <div>
         <Password
@@ -21,20 +25,14 @@ export default class SignUp extends Component {
           handleChange={(e) => {
             this.setState({ field01: e.currentTarget.value });
           }}
-          valid={
-            field01 === field02 &&
-            field01.length + field02.length >= minimumLength * 2
-          }
+          valid={valid}
         />
         <Password
           label="Confirm Password"
           handleChange={(e) => {
             this.setState({ field02: e.currentTarget.value });
           }}
-          valid={
-            field01 === field02 &&
-            field01.length + field02.length >= minimumLength * 2
-          }
+          valid={valid}
         />
         <p className="small">minimum length {this.props.minimumLength}</p>
       </div>
