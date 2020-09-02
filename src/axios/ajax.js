79993b59245d367dd3b449baf4094f8d1ab94cac
Will <will_leighton@outlook.com>
2020-09-02 14:08:04 +0100

    comments working

--- src/axios/ajax.js
+++ src/axios/ajax.js
@@ -1,20 +1,48 @@
-import Axios from "./axios";
 import axios from "./axios";
 
-axios // get article summary
-  .get("/blog/articles")
-  .then((response) => console.log(response.data.data))
-  .catch((error) => console.log(error));
+// delete article;
+// axios.delete("/blog/articles/58").then(() => {
+//   console.log("deleted");
+// });
 
-axios //post new article
-  .post("/blog/articles", {
-    title: "Axios is Boss",
-    content: "It is so great, what fun, wowee!",
-    tags: ["fun", "axios", "boss"],
-  })
-  .then(
-    (response) =>
-      axios
-        .get(`/blog/articles/${response.data.data.id}`) //fetch article using response id
-        .then((response) => console.log(response.data.data)) // log article
-  );
+// axios // get article summary
+
+//   .get("/blog/articles")
+//   .then((response) => console.log(response.data.data))
+//   .catch((error) => console.log(error));
+
+// axios //post new article
+
+//   .post("/blog/articles", {
+//     title: "Axios is Boss",
+//     content: "It is so great, what fun, wowee!",
+//     tags: ["fun", "axios", "boss"],
+//   })
+//   .then(
+//     (response) =>
+//       axios
+//         .get(`/blog/articles/${response.data.data.id}`) //fetch article using response id
+//         .then((response) => console.log(response.data.data)) // log article
+//   );
+
+//update then log tags
+
+// axios
+//   .patch("/blog/articles/59", {
+//     tags: ["fun", "axios", "boss", "Updated YO!"],
+//   })
+//   .then((response) => console.log(response.data.data.tags));
+
+//add a comment to an article and log its id
+// axios
+//   .post("/blog/articles/59/comments", {
+//     email: "will@will.com",
+//     comment: "nice",
+//   })
+//   .then((response) => console.log(response.data.data.id));
+
+// list comments from an article
+axios.get("/blog/articles/59/comments").then((response) => {
+  let comments = response.data.data.map((comment) => comment.comment);
+  console.log(comments);
+});
