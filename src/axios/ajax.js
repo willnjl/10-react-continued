7dcbac046af5bf17dc46aa09aea7c1bd57deb389
Will <will_leighton@outlook.com>
2020-09-02 13:07:41 +0100

    fetch article challenge working

--- src/axios/ajax.js
+++ src/axios/ajax.js
@@ -1,7 +1,20 @@
 import Axios from "./axios";
 import axios from "./axios";
 
-axios
+axios // get article summary
   .get("/blog/articles")
   .then((response) => console.log(response.data.data))
   .catch((error) => console.log(error));
+
+axios //post new article
+  .post("/blog/articles", {
+    title: "Axios is Boss",
+    content: "It is so great, what fun, wowee!",
+    tags: ["fun", "axios", "boss"],
+  })
+  .then(
+    (response) =>
+      axios
+        .get(`/blog/articles/${response.data.data.id}`) //fetch article using response id
+        .then((response) => console.log(response.data.data)) // log article
+  );
